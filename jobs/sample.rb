92b967bfc531d82fb794f405c9e1d0b9f99f52ce
cpeter9 <cpeter9@gmail.com>
2013-08-17 19:56:01 -0400

    Pulling just latest earthquake

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -8,17 +8,11 @@ max_place = ''
 last_karma = 0
 
 SCHEDULER.every '30s' do
-  data = HTTParty.get('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').to_hash["features"]
+  last_karma = current_karma
+  data = HTTParty.get('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson').to_hash["features"]
 
-  data.each_with_index do |element, index|
-    current_karma = element["properties"]["mag"]
-    current_place = element["properties"]["place"]
-  if current_karma > max_karma
-      last_karma = max_karma
-      max_karma = current_karma
-      max_place = current_place
-    end
-  end
+    current_karma = data.first["properties"]["mag"]
+    current_place = data.first["properties"]["place"]
 
   send_event('karma', { current: max_karma, last: last_karma, place: max_place })
 end
