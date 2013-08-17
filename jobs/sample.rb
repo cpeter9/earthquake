5cbd803e84116e42838792ef241fce530bb970a7
cpeter9 <cpeter9@gmail.com>
2013-08-17 19:22:08 -0400

    Adding in city location

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -4,17 +4,21 @@ require 'pry-debugger'
 
 current_karma = 0
 max_karma = 0
+max_place = ''
+last_karma = 0
 
-SCHEDULER.every '20s' do
-  last_karma = max_karma
-  data = HTTParty.get('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson').to_hash["features"]
+SCHEDULER.every '30s' do
+  data = HTTParty.get('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').to_hash["features"]
 
   data.each_with_index do |element, index|
     current_karma = element["properties"]["mag"]
-    if current_karma > last_karma
+    current_place = element["properties"]["place"]
+  if current_karma > max_karma
+      last_karma = max_karma
       max_karma = current_karma
+      max_place = current_place
     end
   end
 
-  send_event('karma', { current: max_karma, last: last_karma })
+  send_event('karma', { current: max_karma, last: last_karma, place: max_place })
 end
